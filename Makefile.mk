0e2b00ff77a96410d65bb72dacda7893d2463216
BilalAzhar0 <96930930+BilalAzhar0@users.noreply.github.com>
2022-08-12 09:54:27 +0500

    Cleaned and added comments to package manifest file.

--- Makefile.mk
+++ Makefile.mk
@@ -1,5 +1,6 @@
 include $(TOPDIR)/rules.mk
-
+# Name, version and release number
+# The name and version of your package are used to define the variable to point to the build directory of your package: $(PKG_BUILD_DIR)
 PKG_NAME:=example1
 PKG_VERSION:=1.0.0
 PKG_RELEASE:=$(AUTORELEASE)
@@ -9,7 +10,7 @@ PKG_LICENSE:=CC0-1.0
 
 include $(INCLUDE_DIR)/package.mk
 include $(INCLUDE_DIR)/cmake.mk
-
+# Package definition; instructs on how and where our package will appear in the overall configuration menu ('make menuconfig')
 define Package/example1
 	SECTION:=utils
 	# Select package by default
@@ -18,16 +19,16 @@ define Package/example1
 	TITLE:=Some different dummy application.
 	URL:=https://www.example.com
 endef
-
+# Package description; a more verbose description on what our package does
 define Package/example1/description
 	This is some dummy application.
 endef
-
+# Package preparation instructions; create the build directory and copy the source code. 
 define Build/Prepare
 	mkdir -p $(PKG_BUILD_DIR)
 	$(CP) ./src/* $(PKG_BUILD_DIR)/
 endef
-
+# Package install instructions; create a directory inside the package to hold our executable, and then copy the executable cmake will build into the folder
 define Package/example1/install
 	$(INSTALL_DIR) $(1)/usr/bin
 	$(INSTALL_BIN) $(PKG_BUILD_DIR)/example1 $(1)/usr/bin/ # Verify this path from past tested example
